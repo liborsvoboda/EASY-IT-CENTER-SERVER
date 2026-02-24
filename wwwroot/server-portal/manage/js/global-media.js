921631699892dbabdc9aea7035b214561f26ac5f
liborsvoboda <libor.svoboda@kliknetezde.cz>
2026-02-24 15:01:02 +0100

    upd

--- wwwroot/server-portal/manage/js/global-media.js
+++ wwwroot/server-portal/manage/js/global-media.js
@@ -1,5 +1,4 @@
 ﻿//Record Audio
-
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 window.URL = window.URL || window.webkitURL;
